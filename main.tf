4bb4706caada392893f6fe18034ff8d9f653de67
chintan-hashi <99983010+chintan-hashi@users.noreply.github.com>
2023-07-07 18:29:28 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "vault_azure_access_credentials" "creds" {
+  role    = "edu-app"
+  backend = "azure"
+}
+
+output "azure_clientid" {
+  value = data.vault_azure_access_credentials.creds.client_id
+}
+
+output "azure_clientsecret" {
+  value = data.vault_azure_access_credentials.creds.client_secret
+}
